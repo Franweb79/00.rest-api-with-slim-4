3e0dbf03084946c7c0cfff2f7536e3df9ff891c7
FranWeb79 <franprietogutweb@gmail.com>
2021-06-01 17:03:13 +0200

    created route with dynamic user name

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# on .gitignore we will ignore docs (private guides) and vendor cause it will make sync much bigger. If you have tyo use the RES #API you should install all the libraries and packages specified on composer.json wirh the command
+# on .gitignore we will ignore 
+
+# -docs (private guides) 
+
+# -vendor cause it will make sync much bigger. If you have to use this REST API you should install all the libraries and packages specified on composer.json with the command composer install once composer is installed on your equip. https://getcomposer.org/
+
+# src/config/db-connection.php. sensible data you can learn hot to do here https://medium.com/@sizil.krishna23/creating-a-rest-api-with-slim-php-framework-mercurial-2829cb3f22b3
 
 # all rights reserved
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+    
+    //this are namespaces
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Selective\BasePath\BasePathMiddleware;
@@ -7,9 +9,11 @@
     use Slim\Factory\AppFactory;
     use Selective\BasePath\BasePathDetector;
 
+    //include the SLIM framework
+
     require_once __DIR__ . '/../vendor/autoload.php';
 
-
+    //create SLIM instance, on slim 4 is so:
 
     $app = AppFactory::create();
 
@@ -25,7 +29,7 @@
     */
 
     /*and added this*/
-    $app->setBasePath("/00.rest-slim-test/public");
+    $app->setBasePath("/00.rest-api-with-slim-4/public");
     $app->addErrorMiddleware(true, true, true);
 
     // Define app routes
@@ -33,6 +37,28 @@
         $response->getBody()->write("Hello, world!");
         return $response;
     });
+
+   /* $app->get('/users', function ($request, $response, array $args) {
+
+        $response->getBody()->write("path to get users");
+        return $response;
+        
+    });*/
+
+    //test dymanic names ans so, still not with database
+    $app->get('/users/{name}', function ($request, $response, array $args) {
+
+        //capture the name
+        
+        $userName = $request->getAttribute('name');
+
+        $response->getBody()->write("hi there ".$userName);
+        return $response;
+        
+    });
+
+
+    
 
     // Run app
     $app->run();
