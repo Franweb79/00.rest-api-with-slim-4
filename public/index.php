30715a80b6aaf7f6ef05685c165514ae620af2a2
FranWeb79 <franprietogutweb@gmail.com>
2021-06-13 01:17:55 +0200

    added boostrap, must find more elegant way

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
+<html>
+    <head>
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    </head>
+    <body></body>
+</html>
+
+
 <?php
     
     //this are namespaces
@@ -36,6 +47,11 @@
 
     require '../src/routes/login.php';
     require '../controllers/login-control.php';
+
+    
+   
+
+
 
 
 
