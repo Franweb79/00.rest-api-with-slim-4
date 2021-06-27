f180c44108b2e0e65efc970e077700b31ba380bf
FranWeb79 <franprietogutweb@gmail.com>
2021-06-28 01:09:13 +0200

    some comments on close-session-control added>

--- controllers/close-session-control.php
+++ controllers/close-session-control.php
@@ -7,7 +7,7 @@
 
     $app->post('/close-session-control', function (Request $request, Response $response, $args){
 
-        
+        session_start();
 
         $data=$request->getParsedBody();
 
@@ -16,7 +16,7 @@
         $response->getBody()->write($jsondata);
 
 
-        return $response;
+        return $response; //with header al principal, y en el principal controlar que si llega algo de aqui, poner bandera de session cerrada
     } );
 
 ?>
