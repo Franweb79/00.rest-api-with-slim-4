7f9a6f5cbbfa041ba9515694cf36b328a75e9342
FranWeb79 <franprietogutweb@gmail.com>
2021-07-01 03:42:43 +0200

    seems valitron library works for the email, nor rest of fields

--- controllers/register-control.php
+++ controllers/register-control.php
@@ -3,15 +3,32 @@
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Psr\Http\Message\ResponseInterface as Response;
 
+    use Valitron\Validator as V;
+
     $app->post('/register-control', function( Request $request, Response $response){
 
        var_dump($request->getParsedBody());
 
+      
+
        $data=$request->getParsedBody();
 
-       var_dump($data["register-email-input-name"]);
+      // $v = new Valitron\Validator($_POST);
+
+      $v = new Valitron\Validator($data);
+
+       $v->rule('email', 'register-email-input-name');
+
+       //var_dump($data["register-email-input-name"]);
+
+       if($v->validate()) {
+            echo "Yay! We're all good!";
+        } else {
+        // Errors
+            print_r($v->errors());
+        }
        
-        var_dump($response->getbody()->write("hola"));
+       // var_dump($response->getbody()->write("hola"));
 
         return $response;
 
