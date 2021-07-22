0171568b47b28e904a09c12a8124fc1171aee1b5
FranWeb79 <franprietogutweb@gmail.com>
2021-07-22 18:15:35 +0200

    working show and hide password on login

--- public/js/my-scripts.js
+++ public/js/my-scripts.js
@@ -1,9 +1,51 @@
 //alert("test js");
 
+var isHidden=true; //when we load the script is always hidden
+
 $( "#test-button" ).click(function( event ) {
     alert( event.currentTarget === this ); // true
   });
 
-  $( "#eye-icon" ).click(function( event ) {
-    alert( event.currentTarget === this ); // true
+$( "#eye-icon" ).click(( event )=> {
+   // alert( event.currentTarget === this ); // true
+
+    
+
+    toogleHideShowPass();
+
+    console.log(isHidden);
+
+    
   });
+
+ /*
+    will be called when clicked the eye icon to show or hide the password, changing the input property
+    @params p_is_hidden : a boolean to determine is pass must be shown or hidden
+
+  */
+  function toogleHideShowPass(){
+
+
+    if(isHidden==true){
+
+      $("#loginPasswordInputID").prop("type", "text");
+
+      isHidden=false;
+
+
+    }else{
+
+      $("#loginPasswordInputID").prop("type", "password");
+
+      isHidden=true;
+
+
+
+    }
+
+   
+
+
+    console.log ("la variable es"+isHidden);
+
+  }
